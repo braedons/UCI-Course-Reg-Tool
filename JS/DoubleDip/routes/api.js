aa495b45ce2f1cd0a85cefa4b60d3b74f03486d8
Braedon Schmidt <braedonaschmidt@gmail.com>
2020-10-18 15:02:16 -0700

    Created structure for finding classes in common

--- JS/DoubleDip/routes/api.js
+++ JS/DoubleDip/routes/api.js
@@ -1,10 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
-	var cat1 = res.get('cat1');
-	var cat2 = res.get('cat2');
-	res.send(cat1 + " " + cat2);
+comparandMap = {'GE': new Set(['cs 1', 'cs 2']), 'CS': new Set(['cs 1', 'cs 3'])}
+
+router.get('/overlap', (req, res, next) => {
+	var comparand1 = req.query.comparand1;
+	var comparand2 = req.query.comparand2;
+
+	if (comparand1 == null || comparand2 == null) {
+		res.send("missing comparands");
+		return;
+	}
+
+	try {
+		overlapList = findOverlap(comparand1, comparand2);
+
+		res.send({
+			comparand1: comparand1,
+			comparand2: comparand2,
+			overlapList: overlapList
+		});
+	}
+	catch (e) {
+		res.send("ERROR: " + e);
+	}
 });
+
+function getClassList(comparand) {
+	return comparandMap[comparand];
+}
+
+function findOverlap(comparand1, comparand2) {
+	list1 = getClassList(comparand1);
+	list2 = getClassList(comparand2);
+
+	if (list1 == null || list2 == null) {
+		throw "Comparands weren't found";
+	}
+
+	return [...list1].filter(elem => list2.has(elem));
+}
 
 module.exports = router;
